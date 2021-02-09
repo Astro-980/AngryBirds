7bfbde2cdff5e5313270ac88709589f4a1203076
Astro-980 <vihaanimation@gmail.com>
2021-02-09 15:44:57 +0530

    sling shot added

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
 var log6;
-var chain;
+var slingshot;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -38,9 +38,8 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-    log6= new Log(230,180,100,PI/2);
 
-    chain= new Chain(bird.body , log6.body);
+    slingshot= new SlingShot(bird.body ,{x:200,y:50});
 
 }
 
@@ -64,6 +63,15 @@ function draw(){
 
     bird.display();
     platform.display();
-    log6.display();
-    chain.display();
+    slingshot.display();
+}
+
+function mouseDragged(){
+    // bird.body.position.x = mouseX;
+    // bird.body.position.y = mouseY;
+    Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY});
+
+}
+function mouseReleased() {
+    slingshot.fly();
 }
