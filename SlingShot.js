5b4c6e8c6e9230eb561991e833c132a84e76c774
Astro-980 <vihaanimation@gmail.com>
2021-02-12 16:47:17 +0530

    sling shot made

--- SlingShot.js
+++ SlingShot.js
@@ -1,25 +1,43 @@
-class SlingShot{
-    constructor(body1,point2){
-        var options={
-            bodyA : body1,
-            pointB : point2,
-            length : 10,
-            stiffness : 0.04
-         }
-         this.sling= Constraint.create(options);
-         this.pointB = point2;
-         World.add(world,this.sling);
+class SlingShot {
+    constructor(body1, point2) {
+        var options = {
+            bodyA: body1,
+            pointB: point2,
+            length: 10,
+            stiffness: 0.04
+        }
+        this.sling = Constraint.create(options);
+        this.pointB = point2;
+        this.sling1 = loadImage("sprites/sling1.png");
+        this.sling2 = loadImage("sprites/sling2.png");
+        this.sling3 = loadImage("sprites/sling3.png");
+        World.add(world, this.sling);
     }
-    display(){
-        if(this.sling.bodyA){
+    display() {
+        image(this.sling1, 200, 20);
+        image(this.sling2, 170, 20);
+        if (this.sling.bodyA) {
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x,pointA.y,pointB.x,pointB.y);
+            push();
+            stroke("#301608");
+            if(this.sling.bodyA.position.x<220){
+                strokeWeight(8);
+                line(pointA.x - 20, pointA.y, pointB.x - 10, pointB.y);
+                line(pointA.x - 20, pointA.y, pointB.x + 30, pointB.y);
+                image(this.sling3, pointA.x - 30, pointA.y - 10, 15, 30);
+            }
+            else{
+                strokeWeight(4);
+                line(pointA.x + 20, pointA.y, pointB.x - 10, pointB.y);
+                line(pointA.x + 20, pointA.y, pointB.x + 30, pointB.y);
+                image(this.sling3, pointA.x + 25, pointA.y - 10, 15, 30);
+            }
+            pop();
         }
     }
-    
-    fly(){
+
+    fly() {
         this.sling.bodyA = null;
     }
 }
